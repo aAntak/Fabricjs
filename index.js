ecb98da84aca5d7d8f150540668026b218f7c5e1
aAntak <andriusk1997@gmail.com>
2020-08-20 09:07:04 +0300

    Color Picker, Clearing canvas, ability to add objects

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("I am index.js");
-
 //new canvas constructor lets fabric create this class instance
 // so we can use the provided fabric.js methods
 const initCanvas = (id) => {
@@ -17,7 +15,8 @@ const setBackground = (url, canvas) => {
 }
 
 const modes = {
-    pan: 'pan'
+    pan: 'pan',
+    drawing: 'drawing'
 }
 
 
@@ -31,32 +30,84 @@ const setPanEvents = (canvas) => {
         const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
         canvas.relativePan(delta);
         console.log(delta);
-    }
+    } 
 });
     
     canvas.on("mouse:up", (e) => {
         mousePressed = false;
+        //canvas.isDrawingMode = false;
     });
 
     canvas.on("mouse:down", (e) => {
         mousePressed = true;
+        
 
     });
 }
 
-const togglePan = () => {
-    if(currentMode ===  modes.pan){
+const toggleMode = (mode) => {
+    if(currentMode === mode) {
         currentMode = '';
-    } else {
-        currentMode = modes.pan;
+        canvas.isDrawingMode = false;
+        return;
     }
+    currentMode = mode;
+    if(currentMode != modes.drawing) {
+        canvas.isDrawingMode = false;
+    }
+    else if(currentMode === modes.drawing) {
+        canvas.isDrawingMode = true;
+        canvas.freeDrawingBrush.color =  color;
+        canvas.renderAll();
+    }
+    canvas.renderAll();
+    
+}
+
+const setColorListener = () => {
+    const picker = document.getElementById("colorPicker");
+    picker.addEventListener('change', (event) =>
+    {
+        color = event.target.value;
+        canvas.freeDrawingBrush.color =  color;
+    })
+}
+
+const clearCanvas = (canvas) => {
+    canvas.getObjects().forEach((o) => {
+        if(o !== canvas.backgroundImage) {
+            canvas.remove(o);
+        }
+        
+    });
+}
+
+const createRect = (canvas) => {
+    var rectangle = new fabric.Rect({
+        width: 20, height: 30, fill: 'blue', left: 50, top: 50
+      });
+
+      canvas.add(rectangle);
+      canvas.renderAll();
+}
+
+const createCirc = (canvas) => {
+    var circle = new fabric.Circle({
+        radius: 20, fill: 'green', left: 100, top: 100
+      });
+    canvas.add(circle);
+
 }
 
 let currentMode;
 const canvas = initCanvas("canvas");
 setBackground("https://previews.123rf.com/images/svenler/svenler1202/svenler120200004/12413537-glossy-canvas-background-with-vignette.jpg", canvas);
 let mousePressed = false;
+let color = document.getElementById("colorPicker").value;
+console.log(color);
 setPanEvents(canvas);
+setColorListener();
+
 
 
 
